5cf4bdef11852db99b3bb0a534cd552d378bb369
Lam Nhan <lamnhan92@gmail.com>
2019-09-27 12:31:12 +0700

    setup test

---
+++ test/index.spec.ts
@@ -0,0 +1,24 @@
+import { expect } from 'chai';
+import { rewireModule } from '../../../../lamnhan.com/modules/testing/dist/src/index';
+
+class MockedMain {}
+
+async function getServer() {
+  const moduleRewiring = rewireModule(
+    () => import('../src/lib/index'),
+    {
+      '@lib/main': { Main: MockedMain }
+    },
+  );
+  const m = await moduleRewiring.getModule();
+  return m.server();
+}
+
+describe('@lib/index', () => {
+
+  it('#server', async () => {
+    const SERVER = await getServer();
+    expect(SERVER instanceof MockedMain).equal(true);
+  });
+
+});
